ca0c57c07596bf26affb096b71a173f27c946f15
Dominik Barylski <barylski.dominik@gmail.com>
2015-12-16 12:40:30 +0100

    refactor app.js, skeleton portfolio section

--- app/js/app.js
+++ app/js/app.js
@@ -1,83 +1,102 @@
-function mouseHint() {
-	var mouseHintUpdated = false;
-	$('.mousehint-fixed').css('opacity',0).delay(800).animate({opacity:1},1000);
+var app = {
 
-	var params = {
-		mouseHintScaleStart: 1,
-		mouseHintScaleMin: 0.65,
-		mouseHinBottomStart: 0,
-		mouseHinBottomMove: 90,
-		pointTop: 235
-	};
+	mouseHint: function() {
+		var mouseHintUpdated = false;
 
-	if ($(window).width() <= 1400) {
-		params = {
-			mouseHintScaleStart: 0.7,
+		$('.mousehint-fixed')
+			.css('opacity', 0)
+			.delay(800)
+			.animate({ opacity: 1 }, 1000);
+
+		var params = {
+			mouseHintScaleStart: 1,
 			mouseHintScaleMin: 0.65,
-			mouseHinBottomStart: 121,
-			mouseHinBottomMove: 40,
-			pointTop: 164
+			mouseHinBottomStart: 0,
+			mouseHinBottomMove: 90,
+			pointTop: 235
+		};
+
+		if ($(window).width() <= 1400) {
+			params = {
+				mouseHintScaleStart: 0.7,
+				mouseHintScaleMin: 0.65,
+				mouseHinBottomStart: 121,
+				mouseHinBottomMove: 40,
+				pointTop: 164
+			}
 		}
+
+		$(window).scroll(function() {
+			var wScroll = $(this).scrollTop();
+
+			if ( ! mouseHintUpdated || wScroll < $('.welcome-section').height()*2) {
+				var mouseHintScale = params.mouseHintScaleStart - (wScroll / 1400);
+				mouseHintScale = mouseHintScale < params.mouseHintScaleMin ? params.mouseHintScaleMin : mouseHintScale;
+
+				var bottom = (wScroll / 10) + params.mouseHinBottomStart;
+				bottom = bottom > params.mouseHinBottomStart + params.mouseHinBottomMove ? params.mouseHinBottomStart + params.mouseHinBottomMove : bottom;
+
+				$('.mousehint-square').css({
+					'transform': 'rotate(-45deg) scale('+mouseHintScale+')',
+					'-webkit-transform': 'rotate(-45deg) scale('+mouseHintScale+')'
+				});
+
+				$('.mousehint-fixed').css({
+					'transform': 'translate(0, '+bottom+'px)'
+				});
+
+				mouseHintUpdated = true;
+			}
+
+			if (wScroll > $('.portfolio-section').offset().top) { // contact section
+				$('.mousehint-fixed').hide();
+				$('.point-fixed').css('margin-top', '100px');
+			} else {
+				$('.mousehint-fixed').show();
+				$('.point-fixed').css('margin-top', params.pointTop + 'px');
+			}
+		});
+	},
+
+	onInit: function() {
+		this.mouseHint();
+
+		$('html').niceScroll({
+			zindex: -1,
+			enablekeyboard: false,
+			spacebarenabled: false
+		});
+
+		$('.main-header').midnight({
+			dataAttr: 'header'
+		});
+
+		$('.point-fixed').midnight({
+			dataAttr: 'point',
+			headerClass: 'midnightPoint',
+			innerClass: 'pointInner'
+		});
+
+		$('.mousehint-fixed').midnight({
+			dataAttr: 'mousehint'
+		});
+
+		AOS.init({
+//			once: true
+		});
+
+
+		$('.mobile-menu-toggle').on('click', function() {
+			$('.mobile-menu-toggle i').toggleClass('transformed');
+//			$('.mobile-nav').toggleClass('show'); TODO mobilenav header class
+		});
 	}
+};
 
-	$(window).scroll(function() {
-		var wScroll = $(this).scrollTop();
-
-		if ( ! mouseHintUpdated || wScroll < $('.welcome-section').height()*2) {
-			var mouseHintScale = params.mouseHintScaleStart - (wScroll / 1400);
-			mouseHintScale = mouseHintScale < params.mouseHintScaleMin ? params.mouseHintScaleMin : mouseHintScale;
-
-			var bottom = (wScroll / 10) + params.mouseHinBottomStart;
-			bottom = bottom > params.mouseHinBottomStart + params.mouseHinBottomMove ? params.mouseHinBottomStart + params.mouseHinBottomMove : bottom;
-
-			$('.mousehint-square').css({
-				'transform': 'rotate(-45deg) scale('+mouseHintScale+')',
-				'-webkit-transform': 'rotate(-45deg) scale('+mouseHintScale+')'
-			});
-
-			$('.mousehint-fixed').css({
-				'transform': 'translate(0, '+bottom+'px)'
-			});
-
-			mouseHintUpdated = true;
-		}
-
-		if (wScroll > $('.portfolio-section').offset().top) { // contact section
-			$('.mousehint-fixed').hide();
-			$('.point-fixed').css('margin-top', '100px');
-		} else {
-			$('.mousehint-fixed').show();
-			$('.point-fixed').css('margin-top', params.pointTop + 'px');
-		}
-	});
-}
 
 $(function() {
-	$('.mobile-menu-toggle').on('click', function() {
-		$('.mobile-menu-toggle i').toggleClass('transformed');
-//		$('.mobile-nav').toggleClass('show'); TODO mobilenav header class
-	});
 
-	$('html').niceScroll({
-		zindex: -1,
-		enablekeyboard: false,
-		spacebarenabled: false
-	});
-
-	$('.main-header').midnight({
-		dataAttr: 'header'
-	});
-
-	$('.point-fixed').midnight({
-		dataAttr: 'point',
-		headerClass: 'midnightPoint',
-		innerClass: 'pointInner'
-	});
-
-	$('.mousehint-fixed').midnight({
-		dataAttr: 'mousehint'
-	});
-
+	// TODO move to technologies.js
 	$('.technologies-carousel').owlCarousel({
 		loop: true,
 		margin: 0,
@@ -98,20 +117,9 @@ $(function() {
 		'aos-duration': '800'
 	});
 
-	mouseHint();
-
-	AOS.init({
-//		once: true
-	});
-
 });
 
 $(function() {
-	welcomeSection.onInit();
-});
-
-$(function() {
-
 	// TODO change section listener
 	$(document).on('onSectionScrollStart', function(e) {
 		console.log('onSectionScrollStart: '+ e.message);
@@ -121,8 +129,10 @@ $(function() {
 		console.log('onSectionScrollEnd: '+ e.message);
 	});
 
+	app.onInit();
 	sections.onInit();
 	sections.onScroll();
+	welcomeSection.onInit();
 });
 
 // TODO
--- app/js/portfolio.js
+++ app/js/portfolio.js
@@ -1,3 +1,22 @@
 var portfolioSection = {
 
+	onInit: function() {
+
+		$.ajax({
+			url: 'http://domons.net/backend/portfolio',
+			dataType: 'json',
+			async: false,
+			success: function(json) {
+				console.log(json);
+			},
+			error: function(error) {
+				console.log(error.statusText + ' ' + error.status);
+			}
+		});
+
+	}
 };
+
+$(function() {
+	portfolioSection.onInit();
+});
